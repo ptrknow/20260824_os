3b52b08296099a8e3152849ae246be9057384da3
Patrick Chan <patrickchan@anywherenetworks.com>
2026-08-31 14:06:58 +0800

    Implement two-level paging

--- src/kernel.c
+++ src/kernel.c
@@ -2,6 +2,7 @@
 #include "idt/idt.h"
 #include "io/io.h"
 #include "memory/heap/kheap.h"
+#include "memory/paging/paging.h"
 #include <stddef.h>
 #include <stdint.h>
 
@@ -58,11 +59,30 @@ void print(const char* str) {
     }
 }
 
+static struct paging_4gb_chunk* kernel_chunk = 0;
+
 void kernel_main() {
     terminal_initialize();
     print("Hello world!\ntest");
 
     kheap_init();
     idt_init();
+
+    kernel_chunk = paging_new_4gb(
+        PAGING_IS_WRITABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
+    paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
+
+    char *ptr = kzalloc(4096); // pointing to virt addr that is linearly mapped to phys addr
+    paging_set(paging_4gb_chunk_get_directory(kernel_chunk), (void*)0x1000,
+        (uint32_t)ptr | PAGING_ACCESS_FROM_ALL | PAGING_IS_PRESENT | PAGING_IS_WRITABLE);
+
+    enable_paging();
+
+    char* ptr2 = (char*)0x1000; // pointing to virtual addr that is mapped by the above paging_set
+    ptr2[0] = 'A';
+    ptr2[1] = 'B';
+    print(ptr2);
+    print(ptr);
+
     enable_interrupts();
 }
