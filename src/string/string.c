04a50c0dc9bc1f99cd982833fd1d36321f6d6e66
Patrick Chan <patrickchan@anywherenetworks.com>
2026-09-21 21:00:11 +0800

    Add strcpy function

--- src/string/string.c
+++ src/string/string.c
@@ -25,3 +25,14 @@ bool isdigit(char c) {
 int tonumericdigit(char c) {
     return c - 48;
 }
+
+char* strcpy(char* dest, const char* src) {
+    char* res = dest;
+    while(*src != 0) {
+        *dest = *src;
+        src += 1;
+        dest += 1;
+    }
+    *dest = 0x00;
+    return res;
+}
