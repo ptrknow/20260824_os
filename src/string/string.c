579f0521d4df5d4befcd515aa90310bfd37a4c07
Patrick Chan <patrickchan@anywherenetworks.com>
2026-09-22 22:29:03 +0800

    Implement string comparison functions

--- src/string/string.c
+++ src/string/string.c
@@ -36,3 +36,50 @@ char* strcpy(char* dest, const char* src) {
     *dest = 0x00;
     return res;
 }
+
+char tolower(char s1) {
+    if (s1 >= 65 && s1 <= 90) {
+        s1 += 32;
+    }
+    return s1;
+}
+
+int strncmp(const char* str1, const char* str2, int n) {
+    unsigned char u1, u2;
+
+    while (n-- > 0) {
+        u1 = (unsigned char)*str1++;
+        u2 = (unsigned char)*str2++;
+        if (u1 != u2)
+            return u1 - u2;
+        if (u1 == '\0') // u1 and u2 has no difference
+            return 0;
+    }
+
+    // first n chars of u1 and u2 have no difference
+    return 0;
+}
+
+int istrncmp(const char* s1, const char* s2, int n) {
+    unsigned char u1, u2;
+
+    while (n-- > 0) {
+        u1 = (unsigned char)*s1++;
+        u2 = (unsigned char)*s2++;
+        if (u1 != u2 && tolower(u1) != tolower(u2))
+            return u1 - u2;
+        if (u1 == '\0')
+            return 0;
+    }
+
+    return 0;
+}
+
+int strnlen_terminator(const char* str, int max, char terminator) {
+    int i = 0;
+    for (i = 0; i < max; i++) {
+        if (str[i] == '\0' || str[i] == terminator)
+            break;
+    }
+    return i;
+}
